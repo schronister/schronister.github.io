d07b52749e061ebb8c94b643399e4f1432444d8e
Steve Chronister <stephen.g.chronister@gmail.com>
2017-01-26 11:15:22 -0600

    style updates

--- app.js
+++ app.js
@@ -1,9 +1,30 @@
-    $(document).ready(function(){
-        $('.parallax').parallax();
-        });
-
-
-    $(document).ready(function(){
-      $('.slider').slider();
+$(document).ready(function(){
+    $('.parallax').parallax();
     });
+
+
+$(document).ready(function(){
+  $('.slider').slider();
+});
         
+$(document).on("click", "#learnmore", function(){
+	$('html, body').animate({
+    scrollTop: $("#projects").offset().top
+  }, 800, function(){
+  });
+});
+
+$(document).ready(function() {
+  $('.toc-wrapper').pushpin({
+    offset: 50
+  });
+  $('.scrollspy').scrollSpy({scrollOffset:0});
+});
+
+$(window).scroll(function(){
+   if( $(document).scrollTop() > 650 ) {
+      $('.toc-wrapper').show();
+   } else {
+      $('.toc-wrapper').hide();
+   }
+});
